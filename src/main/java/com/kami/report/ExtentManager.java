074112c5b1bc37ab76cb3b5e94f7e54c799f1a0b
Dhaval197 <82245220+Dhaval197@users.noreply.github.com>
2023-11-08 23:11:50 -0800

    wait before report gets generated

--- src/main/java/com/kami/report/ExtentManager.java
+++ src/main/java/com/kami/report/ExtentManager.java
@@ -59,6 +59,7 @@ public class ExtentManager {
 			chromeOptions.addArguments("--disable-blink-features", "BlockCredentialedSubresources");
 			chromeOptions.addArguments("--start-fullscreen");
 			chromeOptions.addArguments("--incognito");
+			chromeOptions.addArguments("head");
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver(chromeOptions);
 			Thread.sleep(10000);
